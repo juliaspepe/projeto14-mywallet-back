a22a3e6262ec5601e854d2801721c91e22aa95c7
juliaspepe <devjuliapepe@gmail.com>
2022-11-19 08:26:05 -0300

    feat: create route post sign-in

--- src/index.js
+++ src/index.js
@@ -67,6 +67,26 @@ app.get('/sign-up', async (req, res) => {
     }
 })
 
+app.post("/sign-in", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await db.collection('users').findOne({ email });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+        const token = uuid();
+
+        await db.collection("sessions").insertOne({
+            userId: user._id,
+            token
+        })
+
+        res.send(token);
+        console.log('usuário encontrado')
+    } else {
+        console.log('Usuário não encontrado')
+    }
+});
+
 app.listen(process.env.PORT, () =>
     console.log(`Server running in port: ${process.env.PORT} `)
 )
