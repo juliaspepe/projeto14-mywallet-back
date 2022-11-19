5f2a61faeea390c0f99bb5c6b319b3d2d1b1b292
juliaspepe <devjuliapepe@gmail.com>
2022-11-19 08:24:27 -0300

    feat: create route post sign-up

--- src/index.js
+++ src/index.js
@@ -24,6 +24,40 @@ try {
 
 const db = mongoClient.db("mywallet");
 
+app.post("/sign-up", async (req, res) => {
+    const user = req.body;
+    const { name, email, password } = req.body;
+    const userSchema = joi.object({
+        name: joi.required(),
+        email: joi.string().required(),
+        password: joi.string().required(),
+    });
+
+    try {
+        const userExists = await db.collection('users').findOne({ email: user.email })
+        if (userExists) {
+            return res.status(409).send({ message: "e-mail já cadastrado" })
+        }
+
+        const validation = userSchema.validate({ name, email, password }, { abortEarly: false })
+
+        if (validation.error) {
+            return res.status(422).send(validation.error.details)
+        }
+
+        const passwordHash = bcrypt.hashSync(user.password, 10);
+
+        await db.collection('users').insertOne({ ...user, password: passwordHash })
+
+        res.sendStatus(201);
+        console.log('usuário cadastrado com sucesso')
+
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+});
+
 app.listen(process.env.PORT, () =>
     console.log(`Server running in port: ${process.env.PORT} `)
 )
